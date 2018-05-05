8f31b06be6510d3270c98a8ade633724c0282fff
nando <nando@dessena.it>
2018-05-05 20:22:34 +0200

    + flattened contract versions.

---
+++ scripts/flatten.ps1
@@ -0,0 +1,4 @@
+truffle-flattener .\contracts\ICOStartPromo.sol >.\flat\ICOStartPromo.sol
+truffle-flattener .\contracts\ICOStartSale.sol >.\flat\ICOStartSale.sol
+truffle-flattener .\contracts\ICOStartToken.sol >.\flat\ICOStartToken.sol
+truffle-flattener .\contracts\ICOStartReservation.sol >.\flat\ICOStartReservation.sol
