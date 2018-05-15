771ba898cccb2fe82a2dda46af3245fe22b5338f
nando <nando@dessena.it>
2018-05-15 10:25:23 +0200

    * Gas usage optimization.

--- contracts/ICOStartSale.sol
+++ contracts/ICOStartSale.sol
@@ -219,7 +219,8 @@ contract ICOStartSale is Pausable {
    */
   function _getCurrentPeriod() view internal returns (Period memory _period) {
     _period = Period(0, 0, 0);
-    for (uint256 i = 0; i < periods.length; i++) {
+    uint256 len = periods.length;
+    for (uint256 i = 0; i < len; i++) {
       if ((periods[i].startTimestamp <= block.timestamp) && (periods[i].endTimestamp >= block.timestamp)) {
         _period = periods[i];
         break;
